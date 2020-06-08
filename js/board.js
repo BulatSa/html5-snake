3bb732ab870a76c72e55e269bf45e0384d58e614
Bulat <sadikovba@gmail.com>
2020-06-08 19:10:33 +0300

    Make board

---
+++ js/board.js
@@ -0,0 +1,37 @@
+game.board = {
+  game: game,
+  rows: 15,
+  cols: 15,
+  cells: [],
+
+  create() {
+    this.createCells();
+  },
+
+  createCells() {
+    for (let row = 0; row < this.rows; row++) {
+      for (let col = 0; col < this.cols; col++) {
+        this.cells.push(this.createCell(row, col));
+      }
+    }
+  },
+
+  createCell(row, col) {
+    const cellSize = this.game.sprites.cell.width + 1;
+    const offsetX = (this.game.width - cellSize * this.cols) / 2;
+    const offsetY = (this.game.height - cellSize * this.rows) / 2;
+
+    return {
+      row: row,
+      col: col,
+      x: offsetX + cellSize * col,
+      y: offsetY + cellSize * row,
+    };
+  },
+
+  render() {
+    this.cells.forEach((cell) => {
+      this.game.ctx.drawImage(this.game.sprites.cell, cell.x, cell.y);
+    });
+  },
+};
---
+++ js/game.js
@@ -0,0 +1,53 @@
+const game = {
+  canvas: null,
+  ctx: null,
+  board: null,
+  width: 640,
+  height: 360,
+  sprites: {
+    background: null,
+    cell: null,
+  },
+
+  init() {
+    this.canvas = document.querySelector("#mycanvas");
+    this.ctx = this.canvas.getContext("2d");
+  },
+
+  start() {
+    this.init();
+
+    this.preload(() => {
+      this.run();
+    });
+  },
+
+  preload(callback) {
+    let loaded = 0;
+    const required = Object.keys(this.sprites).length;
+    const onAssetLoad = () => {
+      loaded++;
+      if (loaded >=required) {
+        callback();
+      }
+    }
+
+    for (let key in this.sprites) {
+      this.sprites[key] = new Image();
+      this.sprites[key].src = `img/${key}.png`;
+      this.sprites[key].addEventListener('load', onAssetLoad);
+    }
+  },
+
+  run() {
+    this.board.create();
+    window.requestAnimationFrame(() => {
+      this.ctx.drawImage(this.sprites.background, 0, 0);
+      this.board.render();
+    });
+  },
+};
+
+window.addEventListener("load", () => {
+  game.start();
+});
